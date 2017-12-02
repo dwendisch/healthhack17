abe6b7e1bd45f65a309ee43e4ae4fc26ac6a6e98
jak-ing <lupin2702@gmail.com>
2017-12-02 16:41:51 +0100

    Write OpenCV webcam image to file

--- imageprocessing/read_webcam.py
+++ imageprocessing/read_webcam.py
@@ -3,19 +3,25 @@ import cv2
 cv2.namedWindow("line")
 vc = cv2.VideoCapture(1)
 
+out = cv2.VideoWriter('output.avi', -1, 20.0, (640, 480))
+
 line_thickness = 2
 
 if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
+    status, frame = vc.read()
 else:
-    rval = False
+    status = False
 
-while rval:
+while status:
+    out.write(frame)
     cv2.line(frame,(140,200), (500,200), (0,0,255), line_thickness)
     cv2.line(frame,(140,240), (500,240), (0,0,255), line_thickness)
     cv2.imshow("line", frame)
-    rval, frame = vc.read()
+    status, frame = vc.read()
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
         break
-cv2.destroyWindow("line")
+
+cv2.destroyAllWindows()
+out.release()
+vc.release()

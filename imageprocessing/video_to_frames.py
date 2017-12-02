6ee12e00330b8ce1fbccf7b74e68948a852f3022
jak-ing <lupin2702@gmail.com>
2017-12-02 17:06:31 +0100

    Make video analysis live and add guidelines

--- imageprocessing/video_to_frames.py
+++ imageprocessing/video_to_frames.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+line_thickness = 2
 
 filename = 'winning'
 frames_dirname = 'frames_' + filename
@@ -14,15 +15,17 @@ def extract_frames():
     cv2.namedWindow("bw", flags=cv2.WINDOW_AUTOSIZE)
     cv2.namedWindow("threshold", flags=cv2.WINDOW_AUTOSIZE)
     cv2.namedWindow("blur", flags=cv2.WINDOW_AUTOSIZE)
-    vidcap = cv2.VideoCapture(filename + '.mov')
+    # vidcap = cv2.VideoCapture(filename + '.mov')
+    vidcap = cv2.VideoCapture(1)
 
-    plt.axis([0, 120, 2900, 3300])
+    plt.axis([0, 120, 00, 300])
     plt.ion()
     # plt.show()
     xdata = [0]
     ydata = [2000]
     line, = plt.plot(ydata)
 
+    min = 10000
 
     # for i in range(10):
     #     y = np.random.random()
@@ -40,9 +43,12 @@ def extract_frames():
         success,image = vidcap.read()
         if (not success):
           break
-        cv2.imshow("preview", image)
+        image_presentation = image.copy()
+        cv2.line(image_presentation, (140, 200), (500, 200), (0, 0, 255), line_thickness)
+        cv2.line(image_presentation, (140, 240), (500, 240), (0, 0, 255), line_thickness)
+        cv2.imshow("preview", image_presentation)
 
-        cropped = image[330:390, 620:690]
+        cropped = image[240:280, 140:500]
         cv2.imshow("cropped", cropped)
 
         bw_image = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
@@ -55,29 +61,26 @@ def extract_frames():
         cv2.imshow("blur", blur)
 
         black_count = 60 * 90 - cv2.countNonZero(blur)
-
+        if (black_count < min):
+            min = black_count
         print(black_count)
         # plt.scatter(count, cv2.countNonZero(blur))
         # plt.plot([count])
         xdata.append(count)
-        ydata.append(black_count)
+        ydata.append(black_count - min)
         line.set_xdata(xdata)
         line.set_ydata(ydata)
         # plt.draw()
-        plt.pause(0.5)
-        # plt.pause(0.05)
-        # 
+        # plt.pause(0.5)
+        plt.pause(0.0333)
+        #
 
 
         # key = cv2.waitKey(50)
         # if key == 27:  # exit on ESC
         #   break
         count += 1
-    cv2.destroyWindow("preview")
-    cv2.destroyWindow("bw")
-    cv2.destroyWindow("cropped")
-    cv2.destroyWindow("threshold")
-    cv2.destroyWindow("blur")
+    cv2.destroyAllWindows()
 
 def cutout_image():
     img = cv2.imread(os.path.join(frames_dirname, ("frame%d.jpg" % lower_bound)))

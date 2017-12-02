4ac36d91db325c4351f5990e8cba46003e8ac557
jak-ing <lupin2702@gmail.com>
2017-12-02 16:32:51 +0100

    Put LED guidelines into the image

--- imageprocessing/read_webcam.py
+++ imageprocessing/read_webcam.py
@@ -1,7 +1,9 @@
 import cv2
 
-cv2.namedWindow("preview")
-vc = cv2.VideoCapture(0)
+cv2.namedWindow("line")
+vc = cv2.VideoCapture(1)
+
+line_thickness = 2
 
 if vc.isOpened(): # try to get the first frame
     rval, frame = vc.read()
@@ -9,9 +11,11 @@ else:
     rval = False
 
 while rval:
-    cv2.imshow("preview", frame)
+    cv2.line(frame,(140,200), (500,200), (0,0,255), line_thickness)
+    cv2.line(frame,(140,240), (500,240), (0,0,255), line_thickness)
+    cv2.imshow("line", frame)
     rval, frame = vc.read()
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
         break
-cv2.destroyWindow("preview")
+cv2.destroyWindow("line")
